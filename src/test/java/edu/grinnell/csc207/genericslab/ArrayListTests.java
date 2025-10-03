aba5f15d707b318602430e2cee1ea75e082f0f70
Preston Yoshino <prestonyoshino@Mac.grinnell.edu>
2025-10-02 22:39:32 -0500

    finished part 2

--- src/test/java/edu/grinnell/csc207/genericslab/ArrayListTests.java
+++ src/test/java/edu/grinnell/csc207/genericslab/ArrayListTests.java
@@ -6,5 +6,89 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class ArrayListTests {
-    /* TODO: fill me in! */
+    @Test
+    public void emptyListTest() {
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        assertEquals(0, l.size());
+    }
+
+    @Test
+    public void listAddSize12Nums() {
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        for (int i = 0; i < 12; i++) {
+            l.add(i);
+        }
+        assertEquals(12, l.size());
+    }
+
+    @Test
+    public void listAddNegNums() {
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        l.add(-3);
+        l.add(-10);
+        l.add(-22);
+        assertEquals(-3, l.get(0));
+        assertEquals(-10, l.get(1));
+        assertEquals(-22, l.get(2));
+    }
+
+    @Test
+    public void listGetSeveral() {
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        int[] elements = new int[] {3, 8, 7, 2, 6, 5, 0};
+        for (int i = 0; i < elements.length; i++) {
+            l.add(elements[i]);
+        }
+        for (int i = 0; i < elements.length; i++) {
+            assertEquals(elements[i], l.get(i));
+        }
+    }
+
+    @Test
+    public void listGetOOB() {
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        l.add(3);
+        l.add(2);
+        l.add(7);
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            l.get(-1);
+        });
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            l.get(3);
+        });
+    }
+
+    @Test
+    public void listRemoveSome() {
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        l.add(5);
+        l.add(8);
+        l.add(7);
+        l.add(1);
+        l.add(9);
+        
+        assertEquals(7, l.remove(2));
+        assertEquals(8, l.get(1));
+        assertEquals(1, l.get(2));
+        
+        assertEquals(5, l.remove(0));
+        assertEquals(8, l.get(0));
+
+        assertEquals(9, l.remove(2));
+        assertEquals(1, l.get(1));
+    }
+
+    @Test
+    public void listRemoveOOB() {
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        l.add(5);
+        l.add(8);
+        l.add(7);
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            l.remove(-1);
+        });
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            l.remove(3);
+        });
+    }
 }
